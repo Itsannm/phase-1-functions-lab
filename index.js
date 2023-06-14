6dc3536f86da52faff83c57d0841ff55b8343c33
Itsannm <ann.mwanzia@student.moringaschool.com>
2023-06-15 00:29:31 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(pickupLocation) {
+    if (pickupLocation >= 42) {
+      return pickupLocation - 42;
+    } else {
+      return 42 - pickupLocation;
+    }
+  }
+  function distanceFromHqInFeet(pickupLocation) {
+      const blocks = distanceFromHqInBlocks(pickupLocation);
+      return blocks * 264;
+    }
+    
+    function distanceTravelledInFeet(start, destination) {
+      let distanceInBlocks = destination - start;
+      if (distanceInBlocks < 0) {
+        distanceInBlocks *= -1;
+      }
+      const feetInABlock = 264;
+      const distanceInFeet = distanceInBlocks * feetInABlock;
+      return distanceInFeet;
+    }     
+
+      function calculatesFarePrice(start, destination) {
+        const feetTraveled = Math.abs(destination - start) * 264; // assuming 1 block is 264 feet
+        if (feetTraveled <= 400) {
+          return 0;
+        } else if (feetTraveled > 400 && feetTraveled <= 2000) {
+          return (feetTraveled - 400) * 0.02;
+        } else if (feetTraveled > 2000 && feetTraveled <= 2500) {
+          return 25;
+        } else {
+          return 'cannot travel that far';
+        }
+      }
